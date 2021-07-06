cf1ec285f81b3f6def399e739647d8d511b7b4b0
ivgng <mohamadirvanfajarali@gmail.com>
2021-07-06 09:24:50 +0700

    first commit

---
+++ src/utilities.js
@@ -0,0 +1,17 @@
+export const drawRect = (detections, ctx) => {
+    detections.forEach(prediction => {
+        
+        const [x,y,width,height] = prediction['bbox'];
+        const text = prediction['class'];
+
+        const color = 'green'
+        ctx.strokeStylew = color
+        ctx.font = '18px Arial'
+        ctx.fillStyle = color
+
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+    });
+}
